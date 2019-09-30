6a1bea33fc6edc3c448cd50a6ed84772fb89f7d7
Serhii Kovalov <Serg.kovalyov81@gmail.com>
2019-09-30 23:49:36 +0300

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         WomanWord object1 = new WomanWord(false, "Jane", "Air", 1.50f, 56.3f);
         MansWord object2 = new MansWord(true, "Ivan", "Johnson", 1.89f, 98.7f);
-        System.out.println(object1);
+        /*System.out.println(object1);
         System.out.println(object2);
         System.out.println(object1.toTalk());
         System.out.println(object1.toTolerate());
@@ -17,5 +17,8 @@ public class Main {
         System.out.println(object2.toRelate());
         object1.toBorn();
 
+*/
+        System.out.println(object1.toTalk());
+        System.out.println(object2.toTalk());
     }
 }
